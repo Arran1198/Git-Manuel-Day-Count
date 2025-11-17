48f1d3ce484d411c98e85db749081e5977fbb38f
Mohammad Fallah <mfallah91@yahoo.com>
2025-11-17 21:12:35 +0330

    Add files via upload

---
+++ CppForGit.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    //a -rm os project for git training-- this should be easy right?
+    std::cout << "Hello git" << std::endl;
+    std::cout << "Fellow git may we work togather well" << std::endl;
+    return 0;
+}
